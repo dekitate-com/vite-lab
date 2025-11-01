7019a8c81a50c300acd522e09acf9779ff6503d0
dekitate-com <wonderfulkiyo@gmail.com>
2025-11-01 20:48:52 +0900

    feat: tanstack router の設定を追加

--- apps/with-rsbuild/rsbuild.config.ts
+++ apps/with-rsbuild/rsbuild.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/rspack";
 
 export default defineConfig({
 	plugins: [pluginReact()],
 	source: {
 		entry: {
 			index: "./src/main.tsx",
+		},
+	},
+	tools: {
+		rspack: {
+			plugins: [
+				tanstackRouter({
+					target: "react",
+					autoCodeSplitting: true,
+				}),
+			],
 		},
 	},
 });
